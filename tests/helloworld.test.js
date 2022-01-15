4a69ff5d45f5b2e4966565705a58ff8eabc4944c
toddgunn <todd.k.gunn@gmail.com>
2022-01-15 13:10:46 -0700

    co

--- tests/helloworld.test.js
+++ tests/helloworld.test.js
@@ -1,9 +1,8 @@
-import { expect, it } from "@jest/globals"
+//import { expect, it } from "@jest/globals"
 
-import sayHello from '../utils/helloworld'
+import sayHello from "../utils/helloworld.js"
 
 it("Should say hello",()=>{
     const hello = sayHello();
-
     expect(hello),tobe("hello");
 });
